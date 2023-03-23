cf11b777aa489e06718f0a7690f78766a24943fd
Vincent Fuertez <vfuertez@gmail.com>
2023-03-23 02:20:33 -0400

    updated urls.py

--- Bcontact_backend/urls.py
+++ Bcontact_backend/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from contacts.views import ContactViewset
+
+router = routers.DefaultRouter()
+
+router.register(r'contacts', ContactViewset)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
