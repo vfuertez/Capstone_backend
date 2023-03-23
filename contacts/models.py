57e84821c7b658abb7b6f4bd76e6a5531549b3d7
Vincent Fuertez <vfuertez@gmail.com>
2023-03-23 01:58:19 -0400

    added models

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    url = models.URLField(max_length=200, blank=True, null=True)
+    
